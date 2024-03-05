1bdb817fdef465ce50d4856b9bbf93d870a1dff0
brandon-alexis <brandonmachado159@gmail.com>
2024-03-04 19:52:11 -0500

    adicion de guard para rutas protejidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { loginGuard } from './guards/login.guard';
 
 export const routes: Routes = [
   {
@@ -16,6 +17,7 @@ export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [loginGuard],
     children: [
       {
         path: 'dashboard',
