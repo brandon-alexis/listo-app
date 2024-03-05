04ea65e835ecd31fb3e42783cb0d4e63efa7636a
brandon-alexis <brandonmachado159@gmail.com>
2024-03-04 19:52:55 -0500

    adicion de diseño de layout para rutas protejidas

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { LoginService } from '../../services/login.service';
+import { T_Login } from '../../types/User';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent implements OnInit {
+  user: T_Login;
+
+  constructor(private router: Router, private loginService: LoginService) {
+    this.user = {
+      nombre: 'default',
+      apellido: 'default',
+      login: 'default',
+    };
+  }
+
+  ngOnInit(): void {
+    this.loginService.loginEvent.subscribe((data) => {
+      this.user = data;
+    });
+  }
+
+  onLogout() {
+    this.router.navigateByUrl('/login');
+    localStorage.removeItem('token');
+  }
+}
